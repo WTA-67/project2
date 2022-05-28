7ffc8a2661c2ae45e971dec3e2a21c16a2358e59
Weam Atawy <79086729+WTA-67@users.noreply.github.com>
2022-05-29 00:48:40 +0300

    Update TaskController.js

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,8 +1,11 @@
+const connection = require("express-myconnection");
 function index(req,res){
     res.render('tasks/index');
 
 }
 function create(req,res){
+    console.log(req);
+    res.send("hello word")
     res.render('tasks/create');
 
 }
